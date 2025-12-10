21a1db60671077652552a0cd62d1d216edc59475
Alex_Boborica <alexboborica2@gmail.com>
2025-12-10 19:52:35 +0200

    Logica de adaugare post

--- InfinityFit/Pages/Posts/Create.cs
+++ InfinityFit/Pages/Posts/Create.cs
@@ -1,12 +1,94 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using InfinityFit.Data;
+using InfinityFit.Models;
 using Microsoft.AspNetCore.Authorization;
-namespace InfinityFit.Pages.Posts 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InfinityFit.Pages.Posts
 {
-
-
-    [Authorize]
+    [Authorize] // doar utilizatorii logați pot crea postări
     public class CreateModel : PageModel
     {
-        
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly IWebHostEnvironment _env;
+
+        public CreateModel(ApplicationDbContext context, UserManager<User> userManager, IWebHostEnvironment env)
+        {
+            _context = context;
+            _userManager = userManager;
+            _env = env;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = new InputModel();
+
+        public class InputModel
+        {
+            [Required]
+            public string Title { get; set; }
+
+            [Required]
+            public string Description { get; set; }
+
+            [Required]
+            public IFormFile ImageFile { get; set; }
+
+
+            [Required]
+            public double Latitude { get; set; }
+
+            [Required]
+            public double Longitude { get; set; }
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return Unauthorized();
+
+    
+            string uploadsFolder = Path.Combine(_env.WebRootPath, "uploads");
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(Input.ImageFile.FileName);
+            string filePath = Path.Combine(uploadsFolder, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await Input.ImageFile.CopyToAsync(stream);
+            }
+
+            var post = new Post
+            {
+                UserId = user.Id,
+                User = user,
+                Title = Input.Title,
+                Description = Input.Description,
+                ImagePath = "/uploads/" + fileName,
+                Latitude = Input.Latitude,
+                Longitude = Input.Longitude,
+                DatePosted = DateTime.UtcNow
+            };
+
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Index");
+        }
     }
 }
