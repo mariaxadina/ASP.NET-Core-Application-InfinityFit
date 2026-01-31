631bb0403b88a3ab6699d58560434a2834cf91db
mariaxadina <mariatopliceanu155@gmail.com>
2026-01-31 14:39:49 +0200

    location page

---
+++ InfinityFit/Models/DailyLocation.cs
@@ -0,0 +1,18 @@
+﻿namespace InfinityFit.Models
+{
+    public class DailyLocation
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
